c183434d38993256acff13f0c7bdd113c1a96fc8
SABBIR <sabbirb228@gmail.com>
2023-01-05 04:35:44 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,23 @@
+BG = "black"
+FG = "green"
+
+RD= "blue"
+
+OPEN = "f1"
+CLOSE = "f2"
+General_Cheats = "p"
+Weapons_O = "o"
+Weapons_L = "l"
+Weapons_K = "k"
+police = 'i'
+
+Vehicle_1 = 'z'
+Vehicle_2 = 'x'
+Vehicle_3 = 'c'
+Vehicle_4 = 'v'
+Vehicle_5 = 'b'
+Vehicle_6 = 'g'
+Vehicle_7 = 'h'
+Vehicle_8 = 'j'
+Vehicle_9 = 't'
+Vehicle_0 = 'r'
